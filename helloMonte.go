955fc0b337747b4b0574e98859dbc6ee714d46ed
mark law <markclaw91@gmail.com>
2015-06-21 01:23:19 -0400

    convert to concurrency using gophers

--- helloMonte.go
+++ helloMonte.go
@@ -25,14 +25,12 @@ func serveSingle(pattern string, filename string) {
 
 func main() {
 	r := mux.NewRouter()
-	//r.PathPrefix("/web").Handler(http.FileServer(http.Dir("./web")))
 	r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
 	r.HandleFunc("/monte/{trials}",  helloMonte)
 	
-	//http.HandleFunc("/monte/", helloMonte)
 	fmt.Println("listening...")
 	
-	serveSingle("/favicon.ico", "./favicon.ico")
+	//serveSingle("/favicon.ico", "./web/favicon.ico")
 	
 	http.Handle("/", r)
 
@@ -40,10 +38,6 @@ func main() {
     if err != nil {
       panic(err)
     }
-	
-	//http.ListenAndServe(":8080", nil)
-	//fmt.Println("Hello World!")
-	//fmt.Println(mathutil.CalculatePi(10000))
 }
 
 
@@ -51,7 +45,6 @@ func main() {
 func helloMonte(w http.ResponseWriter, request *http.Request){
 	vars := mux.Vars(request)
 	
-	//value, _ := strconv.ParseInt(r.URL.Path[1:], 0, 32)
 	value, _ := strconv.ParseInt(vars["trials"], 0, 32)
 	
 	montePi := MontePI{value, mathutil.CalculatePi(int(value))}
--- mathutil/mathutil.go
+++ mathutil/mathutil.go
@@ -2,18 +2,56 @@
 package mathutil
 
 import (
-	//"fmt"
+	"fmt"
 	"math/rand"
 	"math"
 	"time"
+	"runtime"
+	//"sync"
 
 )
 
 //func main(){
 //	fmt.Println(calculatePi(1000000))
 //}
-
+//we make this run in parallel
 func CalculatePi(times int) float64{
+	numCPU := MaxParallelism()
+	//fmt.Println(numCPU)
+	//set max cpu to use
+	runtime.GOMAXPROCS(numCPU)
+	//channel to store the return result from go routine
+	inside := make(chan float64, numCPU)
+		
+	eachCoreTrials := times/numCPU
+	//need to add this due to division round down above.
+	lastCoreTrials := eachCoreTrials + (eachCoreTrials*numCPU-times)
+	//fmt.Println(lastCoreTrials)
+	sum:=float64(0)
+	fmt.Println(numCPU)
+	for i:=0;i<numCPU-1;i++ {
+		go taskPi(eachCoreTrials, inside)
+		fmt.Println("taskPi %d is running", i)
+	}
+	
+	//for last core run
+	go taskPi(lastCoreTrials, inside)
+	
+	fmt.Println("finished 1st for loop")
+	for i:=0; i<numCPU; i++ {
+		j:= <- inside
+		fmt.Println("%d", j)
+		sum += j
+	}
+	fmt.Println(sum)
+
+	return sum/float64(times)*4
+}
+
+func taskPi(times int, inside chan float64){
+	
+	//defer wg.Done()
+	
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	sum:=float64(0)
 	if times<0 {
@@ -28,9 +66,11 @@ func CalculatePi(times int) float64{
 			sum++
 		}
 	}
+	inside <- sum
 	//fmt.Println(sum)
 	//fmt.Println(float32(times))
-	return sum/float64(times)*4
+	//return sum, float64(times)
+	fmt.Println("pi finished")
 }
 
 func inSideCircle(x float64, y float64)bool{
@@ -40,4 +80,13 @@ func inSideCircle(x float64, y float64)bool{
 		return false
 	}
 	return true
+}
+//return max core system has
+func MaxParallelism() int {
+    maxProcs := runtime.GOMAXPROCS(0)
+    numCPU := runtime.NumCPU()
+    if maxProcs > numCPU {
+        return maxProcs
+    }
+    return numCPU
 }	
